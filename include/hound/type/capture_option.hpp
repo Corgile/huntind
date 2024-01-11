ae944f4fbebdc7c826c32e028dc6062386911bf8
corgile <mashiro-e@qq.com>
2024-01-11 20:57:45 +0800

    packetTimeout

--- include/hound/type/capture_option.hpp
+++ include/hound/type/capture_option.hpp
@@ -32,7 +32,7 @@ struct capture_option final {
   int32_t duration{-1};
   std::string kafka_config{};
   std::string pcap_file;
-  int packetTimeout;
+  int packetTimeout{20};
   std::string separator;
   char format[8] = {'%', 'l', 'd', ','};
   bool write_file{false};
