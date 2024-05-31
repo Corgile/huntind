e113f4bda4fb31080c8e9176b7a82d2fc37b991a
corgile <mashiro-e@qq.com>
2024-05-31 18:59:49 +0800

    脚本

--- scripts/draw_graph.py
+++ scripts/draw_graph.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-import datetime
 import sys
+import matplotlib.pyplot as plt
 
 # 日志文件路径
 log_file_path = './process_monitor.log'
@@ -10,38 +9,55 @@ timestamps = []
 ram_usage = []
 cpu_usage = []
 
-def main(tick_ : int):
+
+def main(sample: int = 60, tick_: int = 10):
     with open(log_file_path, 'r') as file:
         for line in file:
             parts = line.split()
             if len(parts) < 4: continue
-            # 解析数据
             _ts = f'{parts[0][5:]}-{parts[1]}'
-            ram = float(parts[2]) / 1024
-            #cpu = float(parts[2])
             timestamps.append(_ts)
+            ram = float(parts[2]) / 1024
             ram_usage.append(ram)
-            #cpu_usage.append(cpu)
+
+    # 计算每隔 sample 分钟的行数
+    sample_interval = (sample * 60) // 5  # 每隔 sample 分钟
+    # 只绘制每隔 sample 分钟的数据
+    sampled_timestamps = timestamps[::sample_interval]
+    sampled_ram_usage = ram_usage[::sample_interval]
 
     # 绘制折线图
     plt.figure(figsize=(16, 8))  # 图表大小
-    plt.plot(timestamps, ram_usage, label='RAM Usage (GB)', color='blue')  # RAM使用情况
-    #plt.plot(timestamps, cpu_usage, label='CPU Usage (%)', color='red')  # CPU使用情况
+    plt.plot(sampled_timestamps, sampled_ram_usage, label='RAM Usage', color='green')  # RAM使用情况
+
     plt.xlabel('Timestamp')
-    plt.ylabel('Usage')
-    plt.title('System Resource Usage Over Time')
+    plt.ylabel('Usage (GB)')
+    plt.title(f'RAM Usage Over Time (sample: {sample}min)')
     plt.legend()
     plt.grid(True)
 
-    # 设置横坐标标签间隔
-    plt.xticks(range(0, len(timestamps), tick_), rotation=90)  # 每隔tick_个标签绘制一次
-    plt.tight_layout()  # 自动调整子图参数,使之填充整个图像区域
-    # 保存为PNG
+    # 设置横坐标标签间隔，确保最后一个标签被绘制
+    _tick_interval = (tick_ * 60) // (5 * sample_interval)
+    xticks = list(range(0, len(sampled_timestamps), _tick_interval))
+    if len(sampled_timestamps) - 1 not in xticks:
+        xticks.append(len(sampled_timestamps) - 1)
+    plt.xticks(xticks, rotation=90)
+
+    plt.tight_layout()
     plt.savefig('system_usage.png')
     plt.show()
 
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print(f"usage: python {sys.argv[0]} ticks")
+    if len(sys.argv) < 4:
+        print(f"""
+    usage: python {sys.argv[0]} [sample] [ticks]
+    
+        sample: 每隔 sample 分钟采样一次ram占用
+        ticks:  每隔 ticks 分钟绘制一次x轴label
+        """)
         exit(-1)
-    main(int(sys.argv[1]))
+
+    sample_count = int(sys.argv[1])
+    tick_interval = int(sys.argv[2])
+    main(sample_count, tick_interval)
--- scripts/show-fig.sh
+++ scripts/show-fig.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-
+rm ./process_monitor.log
 scp xhl@host87:~/workspace/flow-encode/cmake-build/process_monitor.log ./
-python draw_graph.py $1
+python draw_graph.py $1 $2
